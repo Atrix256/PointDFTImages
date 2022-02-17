6dd0221f7ee66a3c29870a9561776a4df95fbcdf
Alan Wolfe <alan.wolfe@gmail.com>
2022-02-16 21:07:37 -0800

    need to tile the 2d image better

--- main.cpp
+++ main.cpp
@@ -12,6 +12,12 @@ static const size_t c_1DImageHeight = 10;
 static const size_t c_1DImagePaddingX = 1;
 static const size_t c_1DImagePaddingY = 1;
 
+static const size_t c_2DMaxHz = 10;
+static const size_t c_2DImageWidth = 100;
+static const size_t c_2DImageHeight = 100;
+static const size_t c_2DImagePaddingX = 1;
+static const size_t c_2DImagePaddingY = 1;
+
 static const float c_pi = 3.14159265359f;
 
 template <typename T>
@@ -29,7 +35,7 @@ int main(int argc, char** argv)
 {
     _mkdir("out");
 
-    // make the 1D images
+    // make the 1D image
     {
         const size_t c_actualImageWidth = c_1DImageWidth + c_1DImagePaddingX * 2;
         const size_t c_actualImageHeight = c_1DImageHeight * (c_1DMaxHz+1) + (c_1DMaxHz + 2) * c_1DImagePaddingY;
@@ -54,6 +60,38 @@ int main(int argc, char** argv)
             }
         }
         stbi_write_png("out/1d.png", (int)c_actualImageWidth, (int)c_actualImageHeight, 1, pixels.data(), 0);
+    }
+
+    // make the 2d image
+    {
+        const size_t c_actualImageWidth = c_2DImageWidth + c_2DImagePaddingX * 2;
+        const size_t c_actualImageHeight = c_2DImageHeight * (c_2DMaxHz + 1) + (c_2DMaxHz + 2) * c_2DImagePaddingY;
+        std::vector<unsigned char> pixels(c_actualImageWidth * c_actualImageHeight, 0);
+
+        for (size_t hz = 0; hz <= c_2DMaxHz; ++hz)
+        {
+            size_t imageStart = (hz * (c_2DImageHeight + c_2DImagePaddingY) + c_2DImagePaddingY) * c_actualImageWidth;
+
+            for (size_t y = 0; y < c_2DImageHeight; ++y)
+            {
+                float py = float(y) / float(c_2DImageHeight - 1);
+
+                for (size_t x = 0; x < c_2DImageWidth; ++x)
+                {
+                    float px = float(x) / float(c_2DImageWidth - 1);
+
+                    float re = (cosf(0.0f) + cosf(2.0f * c_pi * float(hz) * px) + cosf(2.0f * c_pi * float(hz) * py)) / 2.0f;
+                    float im = (sinf(0.0f) + sinf(2.0f * c_pi * float(hz) * px) + sinf(2.0f * c_pi * float(hz) * py)) / 2.0f;
+
+                    float mag = sqrtf(re * re + im * im);
+                    unsigned char magc = (unsigned char)Clamp(mag * 256.0f, 0.0f, 255.0f);
+
+                    pixels[imageStart + y * c_actualImageWidth + x + c_2DImagePaddingX] = magc;
+                }
+            }
+        }
+        stbi_write_png("out/2d.png", (int)c_actualImageWidth, (int)c_actualImageHeight, 1, pixels.data(), 0);
+
     }
 
     return 0;
